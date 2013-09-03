a63c10dc2e3de5ea954a9b8264b445609b5ad76e
Tarek Ziade <tarek@ziade.org>
2013-09-03 17:57:19 +0200

    wired the reset button

--- gonuts/app.go
+++ gonuts/app.go
@@ -7,20 +7,38 @@ import (
 
 type Context struct {
     Url string
+    Hits map[string]int
 }
 
 
+var hits = make(map[string]int)
 
 func renderIndex(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[1:]
-    c := Context{Url: title}
+    var url = r.URL.Path[1:]
+    if url == "" {
+      url = "/"
+    }
+
+    hits[url] += 1
+    c := Context{Url: url, Hits: hits}
     t, _ := template.ParseFiles("app.tmpl")
     t.Execute(w, c)
 }
 
 
+func resetHits(w http.ResponseWriter, r *http.Request) {
+    // can't do that in one operation ?
+    for i := range hits {
+        delete(hits, i)
+    }
+    http.Redirect(w, r, "/", http.StatusFound)
+}
+
+
+
 func main() {
     http.HandleFunc("/", renderIndex)
+    http.HandleFunc("/reset", resetHits)
     http.ListenAndServe(":8282", nil)
 }
 
